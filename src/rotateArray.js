0d4930bc7e92969f383bc8945d5fcd0b915bb385
prolifick1 <manzosk3@gmail.com>
2021-03-13 18:39:49 -0500

    13MAR2021

--- src/rotateArray.js
+++ src/rotateArray.js
@@ -1,11 +1,11 @@
 let rotateArray = function(a, k) {
   k = k % a.length;
+  
+  reverse(a, 0, a.length - 1);
+  reverse(a, 0, k - 1);
+  reverse(a, k, a.length - 1);
 
-  rotate(a, 0, a.length - 1);
-  rotate(a, 0, k - 1);
-  rotate(a, k, a.length - 1);
-
-  function rotate(a, start, end) {
+  function reverse(a, start, end) {
     while(start < end) {
       let temp = a[start];
       a[start] = a[end];
@@ -15,6 +15,24 @@ let rotateArray = function(a, k) {
     }
   }
 }
+
+// let rotateArray = function(a, k) {
+//   k = k % a.length;
+
+//   rotate(a, 0, a.length - 1);
+//   rotate(a, 0, k - 1);
+//   rotate(a, k, a.length - 1);
+
+//   function rotate(a, start, end) {
+//     while(start < end) {
+//       let temp = a[start];
+//       a[start] = a[end];
+//       a[end] = temp;
+//       start++;
+//       end--;
+//     }
+//   }
+// }
 
 module.exports = {
   rotateArray
