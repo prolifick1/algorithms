c1391205257196e834b1f3e031e42e2cb876bd0c
Luis Manzo <manzosk3@gmail.com>
2021-03-28 13:31:07 -0400

    adds solution to rotateArray

--- src/rotateArray.js
+++ src/rotateArray.js
@@ -1,38 +1,20 @@
 let rotateArray = function(a, k) {
   k = k % a.length;
-  
   reverse(a, 0, a.length - 1);
   reverse(a, 0, k - 1);
   reverse(a, k, a.length - 1);
 
-  function reverse(a, start, end) {
+  function reverse(arr, start, end) {
     while(start < end) {
-      let temp = a[start];
-      a[start] = a[end];
-      a[end] = temp;
+      let temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
       start++;
       end--;
     }
   }
 }
 
-// let rotateArray = function(a, k) {
-//   k = k % a.length;
-
-//   rotate(a, 0, a.length - 1);
-//   rotate(a, 0, k - 1);
-//   rotate(a, k, a.length - 1);
-
-//   function rotate(a, start, end) {
-//     while(start < end) {
-//       let temp = a[start];
-//       a[start] = a[end];
-//       a[end] = temp;
-//       start++;
-//       end--;
-//     }
-//   }
-// }
 
 module.exports = {
   rotateArray
