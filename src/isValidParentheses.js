3171a32133f29d25e9fa96d00f0cc97d8358ce7c
Luis Manzo <manzosk3@gmail.com>
2021-03-29 13:54:04 -0400

    adds solution isValidParentheses

--- src/isValidParentheses.js
+++ src/isValidParentheses.js
@@ -1,17 +1,18 @@
 const isValidParentheses = function (s) {
-    let stack = [];
-    const brackets = new Map([
-      ['[', ']'],
-      ['{', '}'],
-      ['(', ')']
-    ])
-    for (var i = 0; i < s.length; i++) {
-      if (brackets.has(s[i])) {
-        stack.push(s[i]);
-      } else {
-        let val = stack.pop();
-        console.log(` ${s[i]} ... ${brackets.get(val)}` );
-      if(brackets.get(val) !== s[i]) return false;
+  let stack = [];
+  let brackets = new Map([
+    ['[', ']'],
+    ['{', '}'],
+    ['(', ')']
+  ])
+  for(var i = 0; i < s.length; i++) {
+    if(brackets.has(s[i])) {
+      stack.push(s[i]);
+    } else {
+      let char = stack.pop();
+      if(s[i] !== brackets.get(char)) {
+        return false
+      }
     }
   }
   return stack.length === 0;
