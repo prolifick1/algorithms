6d29ee31aa61ab5d254519ecafb212fd869e7826
Luis Manzo <manzosk3@gmail.com>
2021-06-29 19:31:22 -0400

    refactors happy number

--- src/happyNumber.js
+++ src/happyNumber.js
@@ -1,17 +1,13 @@
 function happyNumber(n) {
   let set = new Set();
-
-  while (!set.has(n)) {
+  while (!set.has(n) && n > 0) {
     let sum = 0;
     set.add(n);
-    while (n > 0) {
-      sum += Math.pow((n % 10), 2);
-      n = Math.floor(n / 10);
-    }
+    sum += Math.pow((n % 10), 2);
+    n = Math.floor(n / 10);
     if (sum === 1) {
       return true
     }
-
     n = sum;
   }
   return false;
