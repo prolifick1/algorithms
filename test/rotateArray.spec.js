1854fe8a9f1042d0820338a74a8371476386911a
Luis Manzo <manzosk3@gmail.com>
2021-11-15 21:01:02 -0800

    clean up tests for rotateArray

--- test/rotateArray.spec.js
+++ test/rotateArray.spec.js
@@ -2,23 +2,27 @@ const {
   expect
 } = require('chai');
 
-const rotateArray = require('../src/rotateArray').rotateArray;
+const { rotateArray } = require('../src/rotateArray');
 
 describe('Rotate Array', function () {
   'use strict';
 
   describe('Reversing a single element linked list', function () {
+   it('Input: nums = [1], k = 1, Output: [1]', () => {
+      const nums = [1];
+      expect((rotateArray(nums, 1), nums)).to.eql([1]);
+    });    
+  });
+
+  describe('Reversing linked list with positive number elements', function() {
     it('Input: nums = [0,1], k = 1, Output: [1,0]', function () {
       const nums = [0, 1];
       expect((rotateArray(nums, 1), nums)).to.eql([1, 0]);
-    });
-  });
-
+    }); 
     it('Input: nums = [0,1], k = 1, Output: [1,0]', () => {
-      const nums = [0, 1];
-      expect((rotateArray(nums, 1), nums)).to.eql([1, 0]);
+	const nums = [0, 1];
+	expect((rotateArray(nums, 1), nums)).to.eql([1, 0]);
     });
-
     it('Input: nums = [0,1,2,3], k = 2, Output: [2,3,0,1]', () => {
       const nums = [0, 1, 2, 3];
       expect((rotateArray(nums, 2), nums)).to.eql([2, 3, 0, 1]);
@@ -28,14 +32,18 @@ describe('Rotate Array', function () {
       const nums = [1, 2, 3, 4, 5, 6, 7];
       expect((rotateArray(nums, 3), nums)).to.eql([5, 6, 7, 1, 2, 3, 4]);
     });
+  });
 
-    it('Input: nums = [-1,-100,3,99], k = 2, Output: [3,99,-1,-100]', () => {
-      const nums = [-1, -100, 3, 99];
-      expect((rotateArray(nums, 2), nums)).to.eql([3, 99, -1, -100]);
+   describe('Reversing linked list with negative number elements', function() {
+    it('Input: nums = [-1,-100,-3,-99], k = 2, Output: [-3,-99,-1,-100]', () => {
+      const nums = [-1, -100, -3, -99];
+      expect((rotateArray(nums, 2), nums)).to.eql([-3, -99, -1, -100]);
     });
-
-    it('Input: nums = [1], k = 1, Output: [1]', () => {
-      const nums = [1];
-      expect((rotateArray(nums, 1), nums)).to.eql([1]);
+   });
+  describe('Reversing linked list with negative rotation index', function() {
+    xit('Input: nums = [1, 2, 3, 4, 5], k = -3, Output: [4, 5, 1, 2, 3]', function() {
+      const nums = [1, 2, 3, 4, 5];
+      expect((rotateArray(nums, -3), nums)).to.eql([4, 5, 1, 2, 3]);
     });
+  });
 });
