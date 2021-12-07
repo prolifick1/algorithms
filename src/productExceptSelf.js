7f05ceeee9678638fecd085029091fe5812d749c
Luis Manzo <manzosk3@gmail.com>
2021-12-07 11:57:12 -0800

    resolves divide by zero solution to productExcepSelf

--- src/productExceptSelf.js
+++ src/productExceptSelf.js
@@ -1,14 +1,25 @@
 function productExceptSelf(arr) {
-  let product = 1;
+  let lefts = new Array(arr.length);
+  lefts[0] = 1;
+  for(let i = 1; i < arr.length; i++) {
+    lefts[i] = arr[i-1] * lefts[i-1];
+  }
+
+  let rights = new Array(arr.length);
+  rights[rights.length - 1] = 1;
+  for(let i = rights.length - 2; i >= 0; i--) {
+    rights[i] = arr[i+1] * rights[i+1];
+  }
+
+  let res = new Array(arr.length);
   for(let i = 0; i < arr.length; i++) {
-    product *= arr[i]
+    if(lefts[i] * rights[i] === -0) {
+      res[i] = 0;
+    } else {
+      res[i] = lefts[i] * rights[i];
+    }
   }
-  let products = Array(arr.length).fill(product);
-  let productsExceptSelf = []; 
-  for(let i = 0; i < products.length; i++) {
-   productsExceptSelf.push(products[i] / arr[i]);
-  }
-  return productsExceptSelf;
+  return res;
 }
 
 module.exports = productExceptSelf;
